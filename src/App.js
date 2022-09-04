c82c419fa041ab98347e90a6f40e27efc7a5cb10
nesibeytskn <nesibeyetiskin1@gmail.com>
2022-09-04 19:05:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+
 
 function App() {
+
+  const [todoText, setTodoText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [willUpdateTodo, setWillUpdateTodo] = useState("");
+  useEffect(() => {
+      const todosFromLocalStorage = localStorage.getItem("todos");
+      console.log(todosFromLocalStorage);
+      if(todosFromLocalStorage === null){
+        localStorage.setItem("todos",JSON.stringify([]));
+      }else{
+        setTodos(JSON.parse(todosFromLocalStorage));
+      }
+  }, []);
+  const deleteTodo = (id) => {
+    console.log(id);
+    const filteredTodos = todos.filter(item => item.id !== id);
+    setTodos(filteredTodos);
+    localStorage.setItem("todos", JSON.stringify(filteredTodos)); 
+   };
+  const editTodo = (id) => {
+    console.log(id);
+    setIsEdit(true);
+    const searchedTodo = todos.find((item) => item.id === id);
+    setTodoText(searchedTodo.text);
+  };
+
+  const changeIsDone = (id) => {
+    const searchedTodo = todos.find((item) => item.id === id);
+    const updatedTodo = {
+      ...searchedTodo,
+      isDone: !searchedTodo.isDone,
+    };
+    const filteredTodos = todos.filter(item => item.id !== id);
+    setTodos([updatedTodo, ...filteredTodos]);
+    localStorage.setItem("todos", JSON.stringify([updatedTodo, ...filteredTodos]));
+    console.log(filteredTodos);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (todoText === "") {
+      alert("Todo text can't be empty!");
+      return;
+    }
+    const hasTodos = todos.find((item) => item.text === todoText);
+    console.log(hasTodos);
+    if (hasTodos !== undefined) {
+      alert("You have the todo already");
+      return; 
+    }
+    if (isEdit === true){
+      console.log(willUpdateTodo, " todo'yu güncelleyeceğiz");
+      const searchedTodo = todos.find(item => item.id === willUpdateTodo);
+      const updatedTodo = {
+        ...searchedTodo,
+        text: todoText,
+      }
+      const filteredTodos = todos.filter(item => item.id !== willUpdateTodo)
+      setTodos([...filteredTodos, updatedTodo]);
+      setTodoText("");
+      setIsEdit(false);
+      setWillUpdateTodo("");
+    }else{
+      const newTodo = {
+        id: new Date().getTime(),
+        isDone: false,
+        text: todoText,
+        date: new Date(),
+      };
+      
+      console.log("newTodo", newTodo);
+      setTodos([newTodo, ...todos]);
+      localStorage.setItem("todos", JSON.stringify([...todos, newTodo]));
+      setTodoText("");
+
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center my-5">Todo App</h1>
+     <TodoForm  handleSubmit={handleSubmit} 
+      todoText={todoText}
+      setTodoText={setTodoText}
+      isEdit={isEdit}
+       />
+      {
+        todos.length <= 0 ? (
+          <p className="text-center my-5">You don't have any todos yet.</p>
+        ):(
+          <>
+          {todos.map((item) => (
+            <Todo 
+            item={item} 
+            deleteTodo={deleteTodo} 
+            setIsEdit={setIsEdit} 
+            setWillUpdateTodo={setWillUpdateTodo} 
+            setTodoText={setTodoText} 
+            changeIsDone={changeIsDone}  
+            />
+          ))}
+            
+          </>
+        )}
+      
     </div>
   );
 }
